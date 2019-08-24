b996281aa71f28151c6de16d0ab34f622580c363
Linh Nguyen <redplane_dt@yahoo.com.vn>
2019-08-24 19:30:42 +0800

    - Updated README.md

--- projects/ngrx-message-bus/src/public_api.ts
+++ projects/ngrx-message-bus/src/public_api.ts
@@ -1,6 +1,9 @@
 /*
  * Public API Surface of ngrx-message-bus
  */
+
 export * from './services/implementations/ngrx-message-bus.service';
 export * from './services/interfaces/ngrx-message-bus-service.interface';
 export * from './modules/ngrx-message-bus.module';
+
+export {MESSAGE_BUS_SERVICE_INJECTOR} from './constants/injection-tokens.constant';
