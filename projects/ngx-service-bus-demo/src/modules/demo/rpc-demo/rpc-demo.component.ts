0f1cc403d901ee4ac913926ca88f968d1fb524ff
Linh Nguyen <redplane_dt@yahoo.com.vn>
2021-04-03 17:41:06 +0700

    - Added RPC service - Adding rpc page demo

--- projects/ngx-service-bus-demo/src/modules/demo/rpc-demo/rpc-demo.component.ts
+++ projects/ngx-service-bus-demo/src/modules/demo/rpc-demo/rpc-demo.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
-import {INgRxMessageBusService, MESSAGE_BUS_SERVICE_PROVIDER} from 'ngrx-message-bus';
+import {INgRxMessageBusService, IRpcService, ITypedRpcRequest, MESSAGE_BUS_SERVICE_PROVIDER, RPC_SERVICE_PROVIDER} from 'ngrx-message-bus';
 import {Observable, ReplaySubject, Subject, Subscription} from 'rxjs';
 import {TimeQueryMessageEvent} from '../../../models/time-query.message-event';
 import {GetTimeCommandRequest} from '../../../models/get-time-command-request';
@@ -11,15 +11,27 @@ import * as moment from 'moment';
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'rpc-demo',
-  templateUrl: 'rpc-demo.component.html'
+  templateUrl: 'rpc-demo.component.html',
+  styleUrls: ['rpc-demo.component.css']
 })
 export class RpcDemoComponent implements OnInit, OnDestroy {
 
   //#region Properties
 
+  private readonly _namespace = 'demo-namespace';
+
+  private readonly _method = 'demo-method';
+
+  private _repliedRpcMessage = '';
+
   private _sendingCommand: boolean;
 
-  private _loadedTime: string = '';
+  private readonly _messageRespondTime = 5;
+
+  private _loadedTime = '';
+
+  // Whether message is being sent through rpc service or not.
+  private _sendingMessageInRpcService = false;
 
   // Subscription watch list.
   private _subscription: Subscription;
@@ -36,11 +48,24 @@ export class RpcDemoComponent implements OnInit, OnDestroy {
     return this._loadedTime;
   }
 
+  public get messageRespondTime(): number {
+    return this._messageRespondTime;
+  }
+
+  public get sendingMessageInRpcService(): boolean {
+    return this._sendingMessageInRpcService;
+  }
+
+  public get repliedRpcMessage(): string {
+    return this._repliedRpcMessage;
+  }
+
   //#endregion
 
   //#region Constructor
 
-  public constructor(@Inject(MESSAGE_BUS_SERVICE_PROVIDER) protected messageBusService: INgRxMessageBusService) {
+  public constructor(@Inject(MESSAGE_BUS_SERVICE_PROVIDER) protected messageBusService: INgRxMessageBusService,
+                     @Inject(RPC_SERVICE_PROVIDER) protected rpcService: IRpcService) {
     this._sendingCommand = false;
     this._subscription = new Subscription();
   }
@@ -59,14 +84,27 @@ export class RpcDemoComponent implements OnInit, OnDestroy {
     const hookTimeQuerySubscription = this.messageBusService
       .hookTypedMessageChannel(new TimeQueryMessageEvent())
       .pipe(
-        delay(5 * 1000)
+        delay(this._messageRespondTime * 1000)
       )
       .subscribe((value: GetTimeCommandRequest) => {
         const commandResponse = new GetTimeCommandResponse(value.id);
         this.messageBusService.addTypedMessage(new TimeUpdateMessageEvent(), commandResponse);
       });
 
+    const typedRequest: ITypedRpcRequest<string, string> = {namespace: this._namespace, method: this._method};
+    const hookRpcMessageSubscription = this.rpcService
+      .hookMethodRequestAsync(typedRequest, {
+        skipHistoricalMessages: true
+      })
+      .pipe(
+        delay(5000)
+      )
+      .subscribe(message => {
+        this.rpcService.sendResponse(typedRequest, message.id, 'Message has been resolved.');
+      });
+
     this._subscription.add(hookTimeQuerySubscription);
+    this._subscription.add(hookRpcMessageSubscription);
   }
 
   public ngOnDestroy(): void {
@@ -89,6 +127,18 @@ export class RpcDemoComponent implements OnInit, OnDestroy {
       });
 
     this._subscription.add(loadTimeSubscription);
+  }
+
+  public clickSendMessageInRpcMessage(): void {
+
+    this._sendingMessageInRpcService = true;
+
+    const typedRequest: ITypedRpcRequest<string, string> = {namespace: this._namespace, method: this._method};
+    this.rpcService.sendRequestAsync(typedRequest, 'Hello world')
+      .subscribe(x => {
+        this._sendingMessageInRpcService = false;
+        this._repliedRpcMessage = x;
+      });
   }
 
   protected loadTimeAsync(): Observable<string> {
